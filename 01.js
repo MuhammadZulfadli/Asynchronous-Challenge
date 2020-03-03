a16be67ade53f628454dd58e3538b37be23b3926
MuhammadZulfadli <zulfadlisimatupang48@gmail.com>
2020-03-03 14:53:02 +0700

    add 01.js

---
+++ 01.js
@@ -0,0 +1,9 @@
+const readDir = new Promise((resolve, reject) => {
+    fs.readdir('/', (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+  
+  readDir.then(result => console.log(result))
+         .catch(error => console.log(error));
