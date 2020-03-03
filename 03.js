ec428da40221b167aa107a7215eb0f21dc098ac3
MuhammadZulfadli <zulfadlisimatupang48@gmail.com>
2020-03-03 16:11:21 +0700

    revisi 03.js

--- 03.js
+++ 03.js
@@ -1,10 +1,26 @@
-for (let i = 1; i <= 3; i++) {
-    setTimeout(() => {
+// for (let i = 1; i <= 3; i++) {
+//     setTimeout(() => {
   
-    }, 1000);
-    console.log(i)
+//     }, 1000);
+//     console.log(i)
+//   }
+  
+//   console.log('Done')
+
+for (let i = 1; i <= 3; i++) {
+    console.log(i);
+    const a = () =>{
+      return new Promise(resolv => {
+        setTimeout(() => {
+          resolv()
+        }, 1000);
+        console.log(i)
+      })
+    }
+    await (a);
   }
   
   console.log('Done')
+
 
 
